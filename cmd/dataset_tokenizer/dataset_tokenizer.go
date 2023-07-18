1fe59a1c4861c1df6960c0046dff991852731c48
Wes Brown <wesbrown18@gmail.com>
2023-07-18 12:11:11 -0400

    Fix single file handling when path does not exist.

--- cmd/dataset_tokenizer/dataset_tokenizer.go
+++ cmd/dataset_tokenizer/dataset_tokenizer.go
@@ -33,7 +33,7 @@ type PathInfo struct {
 // returning a slice of PathInfo.
 func GlobTexts(dirPath string) (pathInfos []PathInfo, err error) {
 	// If the path is a file, return it.
-	if stat, statErr := os.Stat(dirPath); statErr != nil && !stat.IsDir() {
+	if stat, _ := os.Stat(dirPath); stat != nil && !stat.IsDir() {
 		return []PathInfo{{
 			Path:    dirPath,
 			Size:    stat.Size(),
